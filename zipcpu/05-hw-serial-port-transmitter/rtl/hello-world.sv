bc9bf6baa76c7ff9a3d595ab1d488645f7b81ecd
RMH-dotcom <ag.ryanhenry@gmail.com>
2026-02-27 12:35:09 +0000

    Wrote o_busy.

--- zipcpu/05-hw-serial-port-transmitter/rtl/hello-world.sv
+++ zipcpu/05-hw-serial-port-transmitter/rtl/hello-world.sv
@@ -8,5 +8,25 @@
 // 4. Simulate a serial port receiver
 
 module hello_world (
-                    input logic i_clk
+                    input logic i_clk,
+                    input logic i_wr,
+                    input logic i_data,
+                    output logic o_busy,
+                    output logic o_uart_tx
 );
+
+  initial {o_busy, state} = {1'b0, IDLE}; // By default, set o_busy to off, and the state to idle
+  always_ff @(posedge i_clk) // Everytime the clock ticks up (0 to 1), execute the logic below
+    if ((i_wr) && (!o_busy)) //If the write is requested, and o_busy is NOT busy,
+      {o_busy, state} <= {1'b1, START}; //o_busy is turned ON, and the state is active
+        else if (state == IDLE) // or
+          {o_busy, state} <= {1'b0, state}; // Ensure  o_busy stays off while waiting in IDLE
+             else if (state < LAST) // or
+               begin
+                 o_busy <= 1'b1; // o_busy is turned ON
+                 state <= state + 1; // Increment to the next state (counting up)
+                          end else
+                            // when the state reaches LAST, stay busy for one more cycle,
+                            // but reset the state to IDLE
+                            {o_busy, state} <= {1'b1, IDLE};
+endmodule // hello_world
