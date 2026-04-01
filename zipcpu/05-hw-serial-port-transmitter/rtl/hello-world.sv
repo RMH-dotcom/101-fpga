92989e886a98ada6af7a73c81369dd22f2b87c34
RMH-dotcom <ag.ryanhenry@gmail.com>
2026-04-02 00:29:40 +0100

    Designed timer block.

--- zipcpu/05-hw-serial-port-transmitter/rtl/hello-world.sv
+++ zipcpu/05-hw-serial-port-transmitter/rtl/hello-world.sv
@@ -11,7 +11,21 @@ module hello_world (
                     output logic o_uart_tx
 );
 
+  logic [26:0] TIMER; // 27 bits (2^27 = 128,000,000, enough for 100M)
+  logic [3:0]  character_index; // 4 bits (2^4 = 16, enough for 16 characters)
+  logic [7:0]  tx_data;
+  logic        tx_stb;
+  logic        tx_busy;
+  initial TIMER = 0;
+  initial character_index = 0;
+  initial tx_stb = 0;
+
+  // Block 1: Counter/timer
+  // if TIMER == 0  -> reset timer
+  // else TIMER - 1 -> resume timer
   always_ff @(posedge i_clk)
-    if (i_clk) begin
-    end
+    if (TIMER == 0)
+      TIMER <= 100_000_000 - 1;
+    else
+      TIMER <= TIMER - 1;
 endmodule // hello_world
