aea801318579c1ada424e5a3cd1f6eeecc52eeb3
RMH-dotcom <ag.ryanhenry@gmail.com>
2026-05-11 15:16:01 +0100

    Generated a VCD trace for waveform verification using GTKwave.

--- 02-spi-controller/sim/tb_spi_master.cpp
+++ 02-spi-controller/sim/tb_spi_master.cpp
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include "Vspi_top.h"
 #include "verilated.h"
+#include "verilated_vcd_c.h"
 
 int main() {
   VerilatedContext *contextp = new VerilatedContext;
   Vspi_top *dut = new Vspi_top(contextp, "TOP");
 
+  // Enable tracing
+  contextp->traceEverOn(true);
+  VerilatedVcdC *tfp = new VerilatedVcdC;
+  dut->trace(tfp, 99);
+  tfp->open("spi.vcd");
+  
   // Reset: start high, pull low (creates negedge), release high
   dut->i_clk   = 0;
   dut->i_rst_l = 1;
@@ -22,13 +29,17 @@ int main() {
 
   // Run clock for 64 cycles
   for (int i = 0; i < 64; i++) {
+    contextp->timeInc(1);
     dut->i_clk = 1;
     dut->eval();
+    tfp->dump(contextp->time());
     if (i == 0) dut->i_tx_dv = 0;
+    contextp->timeInc(1);
     dut->i_clk = 0;
     dut->eval();
-    contextp->timeInc(1);
+    tfp->dump(contextp->time());
   }
+  tfp->close();
 
   printf("Received: 0x%02X\n", (int)dut->o_slave_rx_byte);
   if (dut->o_slave_rx_byte == 0xAB)
