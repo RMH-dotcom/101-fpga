0962864b901937b4e46926ee6a638245850f4faa
RMH-dotcom <ag.ryanhenry@gmail.com>
2026-08-20 07:31:33 +0100

    Created staging registers. Wrote AW and W execution.

--- 04-axi-lite-peripheral/rtl/axi_fir_filter.sv
+++ 04-axi-lite-peripheral/rtl/axi_fir_filter.sv
@@ -19,7 +19,7 @@ module axi_fir_filter
    // our registers are 16-bit and we'll just take i_wdata[15:0]
 
     input               i_clk,
-    input               i_rst,   // keep active low to match the FIR
+    input               i_rst,         // keep active low to match the FIR
 
    // Write address channel (AW)
     input [31:0]        i_awaddr,
@@ -28,7 +28,7 @@ module axi_fir_filter
 
    // Write data channel (W)
     input [31:0]        i_wdata,
-    input [3:0]         i_wstrb, // byte enables
+    input [3:0]         i_wstrb,       // byte enables
     input               i_wvalid,
     output logic        o_wready,
 
@@ -38,21 +38,27 @@ module axi_fir_filter
     input               i_bready,
 
    // Read address channel (AR)
-    input [31:0]        i_araddr, // from AXI request
-    input               i_arvalid, // "DONE" from AXI request
-    output logic        o_arready, // from AXI request
+    input [31:0]        i_araddr,      // from AXI request
+    input               i_arvalid,     // "DONE" from AXI request
+    output logic        o_arready,     // from AXI request
 
    // Read data channel (R)
-    output logic [31:0] o_rdata, // from FIR
+    output logic [31:0] o_rdata,       // from FIR
     output logic [1:0]  o_rresp,
-    output logic        o_rvalid, // "DONE" from FIR
-    input               i_rready // "ACKNOWLEDGE" from AXI
+    output logic        o_rvalid,      // "DONE" from FIR
+    input               i_rready       // "ACKNOWLEDGE" from AXI
    );
   logic               l_coeff_we;
   logic [3:0]         l_coeff_addr;
   logic signed [15:0] l_coeff_data;
   logic signed [15:0] l_sample; // the sample register written by AW/W
   logic signed [15:0] l_result; //wired from FIR's o_result
+
+  // Internal staging registers for independent write tracks
+  logic               l_awready_reg; // AW has arrived, waiting for W
+  logic [31:0]        l_awaddr_reg;  // holds i_awaddr until W arrives
+  logic               l_wready_reg;  // W has arrived, waiting for AW
+  logic [31:0]        l_wdata_reg;   // holds i_wdata until AW arrives
 
   // The factory: fir_filter mining outpost
   fir_filter u_fir_filter
@@ -67,21 +73,75 @@ module axi_fir_filter
     );
 
   // Block 1: Write Address (AW), Write data (W) & Write Response (B)
+  always_ff @(posedge i_clk)
+    if (!i_rst)
+      begin
+        o_wready <= 1'b0;
+        o_awready <= 1'b0;
+        l_wready_reg <= 1'b0;
+        l_awready_reg <= 1'b0;
+        l_awaddr_reg <= 1'b0;
+        l_wdata_reg <= 1'b0;
+        o_bvalid <= 1'b0;
+        o_bresp <= 2'b00;
+        l_sample <= '0;
+        l_coeff_we <= 1'b0;
+        l_coeff_addr <= '0;
+        l_coeff_data <= '0;
+      end // if (!i_rst)
+    else
+      begin
+        // Default strobes down unless intentionally fired
+        l_coeff_we <= 1'b0;
+
+        // Phase 1 (AW): when o_awready = 1'b1, save the address into l_awaddr_reg
+        if (!l_awready_reg && i_awvalid)
+          begin
+            l_awready_reg <= 1'b1;
+            l_awaddr_reg <= i_awaddr;
+            o_awready <= 1'b1;
+          end
+        else
+          o_awready <= 1'b0;
+
+        // Phase 2 (W): when o_wready 1'b1, we save the cargo data into l_wdata_reg
+        if (!l_wready_reg && i_wvalid)
+          begin
+            l_wready_reg <= 1'b1;
+            l_wdata_reg <= i_wdata;
+            o_wready <= 1'b1;
+          end
+        else
+          o_wready <= 1'b0;
+
+        // when o_bvalid && i_bready = 1'b1, clear
+        if (o_bvalid && i_bready)
+          o_bvalid <= 1'b0;
+
+        //Phase 3 (B): when l_awready_reg && l_wready_reg = 1'b1, write
+        if (l_awready_reg && l_wready_reg && !o_bvalid)
+          begin
+            o_bvalid <= 1'b1;
+            l_awready_reg <= 1'b0; // clear AW ready
+            l_wready_reg <= 1'b0; // clear W ready
+          end
+      end // else: !if(!i_rst)
+
   // Block 2: Read Address (AR) & Read Data (R)
 
   // -- IN FACTORIO TERMS --
 
-  // 1. l_result 16-bit to 32-bit
+  // Step 1: l_result 16-bit to 32-bit
   // l_result is a 16-bit signed integer.
   // However, the AXI-Lite Read Data Bus (o_rdata) is 32 bits wide.
-  // We can't just place a 16-bit block into a 32-bit cargo wagon without securing it.
+  // We can't just place a 16-bit block into a 32-bit cargo wagon without securing it
   // or the signed bit formatting will break.
   // {{16{l_result[15]}}, l_result} will replicate the sign bit (bit 15) 16x to fill
   // the upper half of the cargo wagon.
   // -5(16'hFFFB) safely becomes -5 in 32-bit format (32'hFFFFFFFB).
 
 
-  // 2. Read Address Request (AR Phase)
+  // Step 2: Read Address Request (AR Phase)
   // The AXI wrapper sends a schedule (request) train to pick up cargo:
   //
   // Inputs active:
@@ -94,7 +154,7 @@ module axi_fir_filter
   // The AXI wrapper fires the o_arready output high for exactly one clock cycle.
   // "I've acknowledged your request train. It may leave the station."
 
-  // 3. Read (R) Phase
+  // Step 3: Read (R) Phase
   // A unloading train is sent from the wrapper to l_result:
   //
   // Outputs active:
